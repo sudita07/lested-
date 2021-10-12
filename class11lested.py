fa5cfd6fe9b927a83055adaa9f9cb51e2cf0ef80
sudita07 <92347854+sudita07@users.noreply.github.com>
2021-10-11 20:08:33 -0700

    Add files via upload

---
+++ class11lested.py
@@ -0,0 +1,50 @@
+# x=int(input("enter first number"))
+# r=x%2
+# if(r==0):
+#     print("even")
+#     if x>5:
+#         print("great")
+#     else:
+#         print("not so great")
+# else:
+#     print("odd")
+# ...............................
+# x=3
+# if x==1:
+#     print("1")
+# elif x==2:
+#     print("2")
+# elif x==3:
+#     print("3")
+# elif x==4:
+#     print("4")
+# elif x==5:
+#     print("5")
+# else:
+#     print("wrong input")
+# ......................................
+# x=(input("enter number"))
+# l=len(x)
+# if l!=3:
+#     print("enter a 3 digit number")
+# else:
+#     print("the digit is",(int(x)%100//10))
+# ..............................................
+# x=int(input("enter number"))
+# if x>=60:
+#     print("senior citizen")
+# else:
+#     print("not a senior citizen")
+# .......................................
+# x=int(input("enter first number"))
+# y=int(input("enter second number"))
+# if x>y:
+#     print("smaller number is = ",y)
+# else:
+#     print("smaller number is = ",x)
+# ........................................
+# x=int(input("enter first number"))
+# if x%2==0 and x%3==0:
+#     print("number is divisible by 2 and 3")
+# else:
+#     print("number is not divisible by 2 and 3")
